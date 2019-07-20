6b6276b0730efc42f4e8e1bd518c01c1ec1331ab
Lmh-java <2219090094@qq.com>
2019-07-20 21:58:31 +0800

    I have work on a bug these days and this is the job I did

--- MusicDownloaderV1.1/src/com/Lmh/musicDownloader/main/IdCrawler.java
+++ MusicDownloaderV1.1/src/com/Lmh/musicDownloader/main/IdCrawler.java
@@ -33,7 +33,7 @@ public class IdCrawler {
 		// httpGet.addHeader("Referer", "http://music.163.com/");
 		httpGet.addHeader("User-Agent",
 				"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
-		httpGet.addHeader("Cookie",cookie);
+//		httpGet.addHeader("Cookie",cookie);
 		try {
 			response = httpclient.execute(httpGet);
 
